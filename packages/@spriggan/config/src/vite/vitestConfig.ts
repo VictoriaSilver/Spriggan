1b56a242aaae2b7d9cfb2b9badb1188f13717a84
Victoria Silver <VictoriaSilver@protonmail.com>
2023-12-03 20:17:00 +1300

    test: Add `coverage` scripts

--- packages/@spriggan/config/src/vite/vitestConfig.ts
+++ packages/@spriggan/config/src/vite/vitestConfig.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from "vitest/config";
 
+/* istanbul ignore file -- @preserve */
 export const vitestConfig = defineConfig({
 	test: {
 		globals: true,
 		watch: false,
 		passWithNoTests: true,
 		include: ["src/**/*.spec.ts"],
+		typecheck: {
+			checker: "tsc",
+			tsconfig: "tsconfig.spec.json"
+		},
 		coverage: {
-			reportsDirectory: "coverage",
+			all: true,
+			include: ["src/**"],
 			provider: "istanbul",
 			reporter: ["text", "html", "cobertura"]
 		}
